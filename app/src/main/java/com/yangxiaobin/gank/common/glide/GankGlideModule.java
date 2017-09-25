e154baa21c874e35488d41ed0d3e1a32055ad34d
handsomeyang <1154916418@qq.com>
2017-09-25 15:50:41 +0800

    解决backGround 渲染问题

--- app/src/main/java/com/yangxiaobin/gank/common/glide/GankGlideModule.java
+++ app/src/main/java/com/yangxiaobin/gank/common/glide/GankGlideModule.java
@@ -2,9 +2,12 @@ package com.yangxiaobin.gank.common.glide;
 
 import android.content.Context;
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.GlideBuilder;
 import com.bumptech.glide.Registry;
 import com.bumptech.glide.annotation.GlideModule;
 import com.bumptech.glide.integration.okhttp3.OkHttpUrlLoader;
+import com.bumptech.glide.load.engine.cache.LruResourceCache;
+import com.bumptech.glide.load.engine.cache.MemorySizeCalculator;
 import com.bumptech.glide.load.model.GlideUrl;
 import com.bumptech.glide.module.AppGlideModule;
 import java.io.InputStream;
@@ -15,6 +18,14 @@ import java.io.InputStream;
 
 @GlideModule public class GankGlideModule extends AppGlideModule {
 
+  //手动分配缓存  缓存4屏图片，默认是2
+  @Override public void applyOptions(Context context, GlideBuilder builder) {
+    MemorySizeCalculator calculator =
+        new MemorySizeCalculator.Builder(context).setMemoryCacheScreens(4).build();
+    builder.setMemoryCache(new LruResourceCache(calculator.getMemoryCacheSize()));
+  }
+
+  // 更换网络请求为okHttp
   @Override public void registerComponents(Context context, Glide glide, Registry registry) {
     super.registerComponents(context, glide, registry);
     registry.replace(GlideUrl.class, InputStream.class, new OkHttpUrlLoader.Factory());
--- app/src/main/java/com/yangxiaobin/gank/mvp/view/listener/RecyclerViewOnScrollListener.java
+++ app/src/main/java/com/yangxiaobin/gank/mvp/view/listener/RecyclerViewOnScrollListener.java
@@ -5,7 +5,6 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.target.Target;
 import com.orhanobut.logger.Logger;
 import com.yangxiaobin.Constant;
@@ -33,6 +32,7 @@ public class RecyclerViewOnScrollListener extends RecyclerView.OnScrollListener 
   private MainPresenter mPresenter;
   private CacheHelper mCacheHelper;
   private BitmapFactory.Options mOptions;
+  private int mCurrentPos;
 
   public RecyclerViewOnScrollListener(MainPresenter presenter) {
     mPresenter = presenter;
@@ -42,24 +42,22 @@ public class RecyclerViewOnScrollListener extends RecyclerView.OnScrollListener 
   @Override public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
     super.onScrollStateChanged(recyclerView, newState);
     Context context = recyclerView.getContext();
-
     LinearLayoutManager linearLayoutManager = (LinearLayoutManager) recyclerView.getLayoutManager();
     switch (newState) {
-      case RecyclerView.SCROLL_STATE_IDLE:
+      case RecyclerView.SCROLL_STATE_IDLE:// 当不滚动时
         // glide 4.0 针对RecyclerView优化
         GlideApp.with(context).resumeRequests();
-        // 当不滚动时
-        // 渲染背景
-        notifyBackgroundChange();
         //获取最后一个完全显示的ItemPosition
         int totalItemCount = linearLayoutManager.getItemCount();
-        int currentPos = linearLayoutManager.findLastCompletelyVisibleItemPosition();
-        //Logger.e("当前item：" + currentPos);
-        if (currentPos >= 0 && mPresenter.getTotalEntities() != null) {
+        mCurrentPos = linearLayoutManager.findLastCompletelyVisibleItemPosition();
+        // 渲染背景
+        notifyBackgroundChange();
+        Logger.e("当前item：" + mCurrentPos);
+        if (mCurrentPos >= 0 && mPresenter.getTotalEntities() != null) {
           // setToolbar title
-          if (currentPos != totalItemCount - 1) {
+          if (mCurrentPos != totalItemCount - 1) {
             // 防止刷新的时候crash
-            GankDailyDataEntity entity = mPresenter.getTotalEntities().get(currentPos);
+            GankDailyDataEntity entity = mPresenter.getTotalEntities().get(mCurrentPos);
             // 获取日期
             String todayDate = entity.getResults().get福利().get(0).getPublishedAt().split("T")[0];
             // 获取制定日期title
@@ -84,11 +82,6 @@ public class RecyclerViewOnScrollListener extends RecyclerView.OnScrollListener 
    * 使背景模糊
    */
   public void notifyBackgroundChange() {
-    // cardHelper 为null return
-    int currentItemPos = mPresenter.getView().getCurrentItemPos();
-    if (currentItemPos == Constant.RECYCLERVIEW_CARD_HELPER_NULL) {
-      return;
-    }
     // 获取当前展示的图片url
     List<GankDailyDataEntity> totalEntities = mPresenter.getTotalEntities();
     if (totalEntities == null) {
@@ -97,7 +90,7 @@ public class RecyclerViewOnScrollListener extends RecyclerView.OnScrollListener 
     }
     GankDailyDataEntity gankDailyDataEntity;
     try {
-      gankDailyDataEntity = totalEntities.get(currentItemPos);
+      gankDailyDataEntity = totalEntities.get(mCurrentPos);
     } catch (Exception e) {
       e.printStackTrace();
       return;
@@ -113,7 +106,7 @@ public class RecyclerViewOnScrollListener extends RecyclerView.OnScrollListener 
           .subscribeOn(Schedulers.newThread())
           .flatMap(new Function<String, Publisher<File>>() {
             @Override public Publisher<File> apply(String s) throws Exception {
-              return Flowable.just(Glide.with(mPresenter.getView().getViewContext())
+              return Flowable.just(GlideApp.with(mPresenter.getView().getViewContext())
                   .load(url)
                   .downloadOnly(Target.SIZE_ORIGINAL, Target.SIZE_ORIGINAL)
                   .get());
@@ -128,11 +121,6 @@ public class RecyclerViewOnScrollListener extends RecyclerView.OnScrollListener 
                 mOptions.inPreferredConfig = Bitmap.Config.ARGB_4444;
               }
               Bitmap bitmap = BitmapFactory.decodeFile(file.getAbsolutePath(), mOptions);
-              //int byteCount = bitmap.getByteCount();
-              //5.56M   argb 444为2.78 MB   rgb_565 2.78 MB
-              //Logger.e(
-              //    "bitmap size:" + Formatter.formatFileSize(mPresenter.getView().getViewContext(),
-              //        byteCount));
               mCacheHelper.getLruCache().put(url, bitmap);
               mPresenter.getView().startSwitchBgAnim(bitmap);
             }
